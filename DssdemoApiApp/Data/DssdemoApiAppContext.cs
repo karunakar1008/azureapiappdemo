b33fc4f4c0d89090662dead83390e7b8ebb294e9
karunakarbhogyari <karunakar1008@gmail.com>
2022-02-05 19:51:58 +0530

    Add project files.

---
+++ DssdemoApiApp/Data/DssdemoApiAppContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using DssdemoApiApp.Data;
+
+namespace DssdemoApiApp.Models
+{
+    public class DssdemoApiAppContext : DbContext
+    {
+        public DssdemoApiAppContext (DbContextOptions<DssdemoApiAppContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<DssdemoApiApp.Data.Employee> Employee { get; set; }
+    }
+}
---
+++ DssdemoApiApp/Data/Employee.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DssdemoApiApp.Data
+{
+    public class Employee
+    {
+        [Key]
+        public int EmpId { get; set; }
+        public string Name { get; set; }
+        public decimal Salary { get; set; }
+    }
+}
